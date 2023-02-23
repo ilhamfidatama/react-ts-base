e74fe35ef96e5424adabba5fc22e940a8d08cb47
Muhammad Ilham Fidatama <40228147+ilhamfidatama@users.noreply.github.com>
2023-02-23 07:49:02 +0000

    starting create base with hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from "./api/config";
 
 function App() {
+  
+  useEffect(() => {
+    axios.get("/users")
+      .then((response: any) => {
+        console.log(response.data);
+      })
+      .catch((e: Error) => {
+        console.log(e);
+      })
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
